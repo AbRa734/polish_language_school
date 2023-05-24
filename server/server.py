992fe04519275af7dc888ba1ab9d289b65cc6a72
AbRa734 <sz.o@yahoo.com>
2023-05-24 22:20:49 +0200

    json

--- server/server.py
+++ server/server.py
@@ -1,14 +1,31 @@
-from flask import Flask
-from flask_cors import CORS, cross_origin
+import os
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/members")
-@cross_origin()
-def members():
-    return {"members":["Member1", "Member2", "Member3"]}
+handle = ''
+tCount = 0
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/members', methods = ['POST'])
+def result():
+    data = request.form
+    filename = os.path.join(app.static_folder, '/Users/oliwierszafranski/WebstormProjects/ostateczny_projekt_www/data.json')
+
+    with open(filename, 'r+') as file:
+        file_data = json.load(file)
+        file_data["wiadomosci"].append(data)
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
+
+    return jsonify(data)
+
+@app.route('/members', methods = ['GET'])
+def result_get():
+    filename = os.path.join(app.static_folder, '/Users/oliwierszafranski/WebstormProjects/ostateczny_projekt_www/data.json')
+    with open(filename) as blog_file:
+        data = json.load(blog_file)
+        return data
--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React, {useState, useEffect} from 'react'
 
-function App() {
-    const [data, setData] = useState([{}])
-    useEffect(() => {
-        fetch("http://127.0.0.1:5000/members", {
-  "method": "GET",
-  "headers": {
-    "content-type": "application/json",
-    "accept": "application/json"
-  }}).then(
-            res => res.json()
-        ).then(
-            data => {
-                setData(data)
-                console.log(data)
-            }
-        )
-    }, [])
-    return (
-        <div>
-            {(typeof data.members === 'undefined') ? (
-                <p>Loading...</p>
-            ):(
-                data.members.map((member, i) => (
-                    <p key={i}>{member}</p>
-                ))
-            )}
-        </div>
-    )
-}
+function App (){
 
-export default App
+
+    const [initialData, setInitialData] = useState([{}]);
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/members').then(
+            response => response.json()
+        ).then(data => setInitialData(data))
+    }, []);
+
+    return (
+        <div className="App">
+            <h1 className="header">Twitter Sentiment Analysis</h1>
+            <form action="http://127.0.0.1:5000/members" method = "POST">
+                <div className="forms">
+                    <label>
+                        Enter Twitter Handle:
+                        <input type="text" name="handle" />
+                    </label>
+                    <label>
+                        Enter Number of Tweets (200 Limit):
+                        <input type="text" name="tCount"/>
+                    </label>
+                </div>
+                <div className = "button">
+                    <input type="submit" />
+                </div>
+            </form>
+
+        </div>
+    );
+
+}
+export default App;
